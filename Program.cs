fed2424c78e616b261b68af9df89e97cc5ef8d6b
MattiaFang <fangwuchen1@163.com>
2020-03-25 10:05:14 +0800

    first C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Example03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();//输入一个数
+            var x = Convert.ToInt32(s);//字符串类型转换整数
+            for (int i = 0; i < x+1; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
